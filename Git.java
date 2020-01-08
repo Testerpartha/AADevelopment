d88b4a0d82824ac6be1aec8cc6d2112774a3612c
Testerpartha <59641101+Testerpartha@users.noreply.github.com>
2020-01-08 10:20:57 +0000

    Update Git.java

--- Git.java
+++ Git.java
@@ -4,5 +4,6 @@ public static void main(String args[])
 {
 System.out.println("welcome to jenkins");
   System.out.println("welcome to git");
+  System.out.println("welcome to jenkins integration with github");
 }
 }
