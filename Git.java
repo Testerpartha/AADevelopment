4eba2ac92d07c077a13290fa08996d9a27dab93c
Testerpartha <59641101+Testerpartha@users.noreply.github.com>
2020-03-05 09:15:59 +0000

    Update Git.java

--- Git.java
+++ Git.java
@@ -3,6 +3,7 @@ class Sample
 public static void main(String args[])
 {
 System.out.println("welcome to jenkins");
+  System.out.println(""test");
   System.out.println("welcome to git");
   System.out.println("welcome to jenkins integration with github");
    System.out.println("not committed");
